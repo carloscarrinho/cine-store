c47cf347604aff733f6b65ede1d025eb433e898c
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2022-02-18 19:07:03 -0300

    feat: ensure ChooseSeatController calls Notifier with correct values

--- src/presentation/controllers/choose-seat/choose-seat-controller.ts
+++ src/presentation/controllers/choose-seat/choose-seat-controller.ts
@@ -1,3 +1,4 @@
+import { Notifier } from "../../../application/contracts/notifier";
 import { BookSeat } from "../../../application/usecases/book-seat/book-seat";
 import { Controller } from "../../protocols/controller";
 import { badRequest, serverError } from "../../protocols/http-helpers";
@@ -9,6 +10,7 @@ export class ChooseSeatController implements Controller {
   constructor(
     private readonly validation: Validation,
     private readonly bookSeat: BookSeat,
+    private readonly notifier: Notifier,
   ) {}
   
   async handle (request: HttpRequest): Promise<HttpResponse> {
@@ -18,6 +20,14 @@ export class ChooseSeatController implements Controller {
     try {
       const reservation = await this.bookSeat.book(request.body);
       
+      await this.notifier.publish('store_service', JSON.stringify({
+        reservationId: reservation.id,
+        sessionId: reservation.sessionId,
+        seatId: reservation.seatId,
+        personId: reservation.personId,
+        eventType: 'BOOKED_SEAT'
+      }));
+
       return null;
     } catch (error) {
       return serverError(error);
--- tests/unit/presentation/controllers/choose-seat/choose-seat-controller.spec.ts
+++ tests/unit/presentation/controllers/choose-seat/choose-seat-controller.spec.ts
@@ -4,23 +4,30 @@ import { Validation } from "../../../../../src/presentation/protocols/validation
 import { badRequest, serverError } from "../../../../../src/presentation/protocols/http-helpers";
 import { BookSeat } from "../../../../../src/application/usecases/book-seat/book-seat";
 import { Reservation } from "../../../../../src/domain/entities/reservation";
+import { Notifier } from "../../../../../src/application/contracts/notifier";
 
 const makeSut = ({
   validate,
-  book
+  book,
+  publish
 }:{
   validate?: Function,
   book?: Function,
+  publish?: Function,
 }): Controller => {
   const validation = {
     validate: validate ?? jest.fn().mockReturnValueOnce(null)
   } as unknown as Validation
 
   const bookSeat = {
-    book: book ?? jest.fn().mockResolvedValueOnce(null)
+    book: book ?? jest.fn().mockResolvedValueOnce(makeDefaultReservation())
   } as unknown as BookSeat
+
+  const notifier = {
+    publish: publish ?? jest.fn().mockResolvedValueOnce(true)
+  } as unknown as Notifier
   
-  return new ChooseSeatController(validation, bookSeat);
+  return new ChooseSeatController(validation, bookSeat, notifier);
 }
 
 const makeDefaultRequest = (data?: object) => ({
@@ -91,12 +98,34 @@ describe('Unit', () => {
           book: jest.fn().mockImplementationOnce(() => { throw error }) }
         const chooseSeatController = makeSut(dependencies);
         const request = makeDefaultRequest();
-        
+
         // When
         const response = await chooseSeatController.handle(request);
 
         // Then
         expect(response).toStrictEqual(serverError(error))
+      });
+
+      it('Should call Notifier with correct values', async () => {
+        // Given
+        const dependencies = { publish: jest.fn() };
+        const chooseSeatController = makeSut(dependencies);
+        const request = makeDefaultRequest();
+        const reservation = makeDefaultReservation();
+        const queue = 'store_service';
+        const message = JSON.stringify({
+          reservationId: reservation.id,
+          sessionId: reservation.sessionId,
+          seatId: reservation.seatId,
+          personId: reservation.personId,
+          eventType: 'BOOKED_SEAT',
+        });
+
+        // When
+        await chooseSeatController.handle(request);
+
+        // Then
+        expect(dependencies.publish).toHaveBeenCalledWith(queue, message);
       });
     });
   });
